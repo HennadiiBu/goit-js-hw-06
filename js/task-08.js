97a553cdb956bad8f0490146a8d3bc4ab34df8f9
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-27 16:28:03 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSub);
+
+function onSub(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if ((email.value  || password.value) === '') {
+        alert(`Заполните все поля`);
+    }
+
+    const result = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(result)
+}
