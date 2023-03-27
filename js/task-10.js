4dc3b9256f60bf4f1d243db0515bef3cda72f3c0
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-27 18:40:07 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -4,18 +4,32 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const fatherDiv = document.querySelector('.boxes');
+const fatherDiv = document.querySelector('#boxes');
 const createBtn = document.querySelector('[data-create]')
 const destroyBtn = document.querySelector('[data-destroy]')
 const inputValue = document.querySelector('input');
 
 
-createBtn.addEventListener('click', createBoxes)
-  let amount=Number(inputValue.value)
-function createBoxes(amount) {
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-  const mass = [];
-mass.length=amount
-  console.log(mass)
-  // fatherDiv.append(...mass)
+function createBoxes(amount) {
+amount = Number(inputValue.value)
+  const mass = Array(amount);
+    let w = 30;
+    let h = 30;
+  for (let i = 0; i < mass.length; i++){
+
+    mass[i] = `<div class="childDiv" style="width:${w}px; height:${h}px; background-color: ${getRandomHexColor()};">1</div>`;
+    w += 10;
+    h += 10;
+  }
+
+  fatherDiv.insertAdjacentHTML("beforeend", mass.join(''))
 }
+
+function destroyBoxes() {
+  const childDiv = document.querySelectorAll('div#boxes > div')
+  childDiv.forEach(elem=>elem.remove())
+}
+
