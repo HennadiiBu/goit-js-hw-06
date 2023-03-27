6c27a4c1293f05e1b4888915cda971109ce00644
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-27 16:35:12 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBkg = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', onClick);
+
+function onClick() {
+  colorBkg.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = getRandomHexColor();
+}
+
+
