967e7f091c6fc163e1e96dbab766b3071d0a27d1
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-28 11:29:22 +0300

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,17 @@
 const categoryList = document.querySelector(`#categories`);
-console.log(`Number of categories: ${categoryList.children.length}`);
+// console.log(`Number of categories: ${categoryList.children.length}`);
+
 const titleList = document.querySelectorAll(`h2`);
-titleList.forEach(item => {
-    const title = item.textContent
-    console.log(`Category: ${title}`)
-    console.log(`Elements: ${title.length}`)
-})
+
+console.log([...categoryList.children].forEach(item => {
+    // console.log(item)
+    console.log(item.firstElementChild.textContent)
+    console.log(item.lastElementChild.children.length)
+}))
+
+
+// titleList.forEach(item => {
+//     const title = item.textContent
+//     console.log(`Category: ${title}`)
+//     console.log(`Elements: ${title.length}`)
+// })
